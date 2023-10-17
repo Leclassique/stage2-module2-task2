c03dd781942e70e877eae3c7b0a10b37af3daebd
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-17 22:03:10 +0300

    Completed task

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,26 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/user/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse =(HttpServletResponse) servletResponse;
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpSession httpSession = ((HttpServletRequest) servletRequest).getSession();
+        String user = (String) httpSession.getAttribute("user");
+        if (user == null) {
+            httpServletResponse.sendRedirect("/login.jsp");
+            return;
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+
+    }
+
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,23 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+@Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException{
+    HttpSession httpSession = httpServletRequest.getSession();
+    if (httpSession.getAttribute("user") != null) {
+        httpServletResponse.sendRedirect("/user/hello.jsp");
+    } else {
+        httpServletResponse.sendRedirect("/login.jsp");
+    }
 }
+}
+
